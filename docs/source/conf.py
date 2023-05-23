a757b3ff4bf03edf33edb89bf8782ed9b6601e7f
Kunlun Zhu <zhuklun@mail2.sysu.edu.cn>
2023-05-23 13:19:12 +0800

    add

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BMTool'
+copyright = '2023, OpenBMB'
+author = 'OpenBMB Team'
 
 release = '0.1'
 version = '0.1.0'
